4f63c1cb78ac217b730c77e88ffc9e58101b109c
stickm4n <jcgalindo.jcgh@gmail.com>
2023-04-11 12:08:49 -0400

    First commit!

---
+++ Dota2Clusters.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace dota2ping;
+
+public class Cluster
+{
+    public string Id;
+    public string Name;
+    public List<string> Relays = new();
+
+
+    public Cluster(string id, JsonNode data)
+    {
+        Id = id;
+        Name = data.AsObject()["desc"]!.ToString();
+
+        if (!data.AsObject().ContainsKey("relays"))
+            return;
+
+        foreach (var relay in data.AsObject()["relays"]?.AsArray()!)
+        {
+            Relays.Add(relay!.AsObject()["ipv4"]!.ToString());
+        }
+    }
+}
+
+public class Dota2Clusters : List<Cluster>
+{
+    private const string ClustersUri = "https://api.steampowered.com/ISteamApps/GetSDRConfig/v1?appid=570";
+
+    public Dota2Clusters()
+    {
+        try
+        {
+            HttpClient client = new();
+
+            var response = client.GetAsync(ClustersUri).Result;
+            response.EnsureSuccessStatusCode();
+            var responseBody = response.Content.ReadAsStringAsync().Result;
+
+            var pops = JsonSerializer.Deserialize<JsonObject>(responseBody)!.AsObject()["pops"]!;
+            foreach (var e in pops.AsObject())
+                Add(new Cluster(e.Key, e.Value!));
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine("Exception: {0}", e.Message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.NetworkInformation;
+using System.Text;
+using dota2ping;
+
+
+var clusters = new Dota2Clusters();
+
+var pingSender = new Ping();
+
+var buffer = Encoding.ASCII.GetBytes(new string('\0', 32));
+const int timeout = 2;
+
+foreach (var cluster in clusters)
+{
+    Console.WriteLine("{0} [{1}]", cluster.Name, cluster.Id);
+
+    foreach (var relay in cluster.Relays)
+    {
+        var reply = pingSender.Send(relay, timeout, buffer);
+
+        if (reply.Status == IPStatus.Success)
+        {
+            Console.WriteLine("{0} -> {1}ms", reply.Address, reply.RoundtripTime);
+        }
+        else
+        {
+            Console.WriteLine("{0} -> failure", reply.Address);
+        }
+    }
+
+    Console.WriteLine("-------------------------------------");
+}
